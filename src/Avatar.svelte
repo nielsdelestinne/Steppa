86854e5969c4ef00e03211028f98816d682976c0
nielsdelestinne <nielsdelestinne@gmail.com>
2020-04-13 12:49:13 +0200

    Size 600px -> 10 generations

--- src/Avatar.svelte
+++ src/Avatar.svelte
@@ -8,43 +8,53 @@
     let canvasElement;
 
     function draw(ctx: CanvasRenderingContext2D, finalGeneration: Cell[][]) {
+
+        const colours = [
+            ["#000000", "#05715c", "#0aac8b", "#12c19d", "#20d9b3", "#1cdcb5", "#28e7c0", "#34fcd3"],
+            ["#000000", "#5f8900", "#88ba15", "#a4de1e", "#baf52e", "#c1f642", "#d1fc6c", "#daff89"],
+            ["#000000", "#860174", "#cb01ae", "#ff02da", "#ff15dc", "#ff34e0", "#ff4ae4", "#ff71ea"],
+            ["#000000", "#7b3900", "#bb5801", "#e36800", "#ff7a07", "#ff8b2a", "#ff953c", "#ffa14e"]
+        ];
+
+        let colour = colours[Math.floor(Math.random() * colours.length)];
+
         for (let y = 0; y < finalGeneration.length; y++) {
             for (let x = 0; x < finalGeneration[y].length; x++) {
                 ctx.beginPath();
                 ctx.rect(x * 10, y * 10, 10, 10);
-                ctx.fillStyle = getFillStyle(finalGeneration[y][x]);
+                ctx.fillStyle = getFillStyle(finalGeneration[y][x], colour);
                 ctx.fill();
             }
         }
     }
 
-    function getFillStyle(cell: Cell) {
-        if(cell.isAlive && cell.amountOfNeighbours >= 8) {
-            return "#34fcd3";
+    function getFillStyle(cell: Cell, colour: string[]) {
+        if (cell.isAlive && cell.amountOfNeighbours >= 8) {
+            return colour[7];
         }
-        if(cell.isAlive && cell.amountOfNeighbours >= 6) {
-            return "#28e7c0";
+        if (cell.isAlive && cell.amountOfNeighbours >= 6) {
+            return colour[6];
         }
-        if(cell.isAlive && cell.amountOfNeighbours >= 6) {
-            return "#1cdcb5";
+        if (cell.isAlive && cell.amountOfNeighbours >= 6) {
+            return colour[5];
         }
-        if(cell.isAlive && cell.amountOfNeighbours >= 4) {
-            return "#20d9b3";
+        if (cell.isAlive && cell.amountOfNeighbours >= 4) {
+            return colour[4];
         }
-        if(cell.isAlive && cell.amountOfNeighbours >= 2) {
-            return "#12c19d";
+        if (cell.isAlive && cell.amountOfNeighbours >= 2) {
+            return colour[3];
         }
-        if(cell.isAlive) {
-            return "#0aac8b";
+        if (cell.isAlive) {
+            return colour[2];
         }
-        if(!cell.isAlive && cell.amountOfNeighbours >= 1) {
-            return "#05715c";
+        if (!cell.isAlive && cell.amountOfNeighbours >= 1) {
+            return colour[1];
         }
-        return "#2d2d2d";
+        return colour[0];
     }
 
-    export const canvasHeight = 400;
-    export const canvasWidth = 400;
+    export const canvasHeight = 600;
+    export const canvasWidth = 600;
 
     onMount(() => {
         const cellSize = 10;
@@ -53,7 +63,7 @@
 
         const ctx = canvasElement.getContext('2d');
         const avatarGeneration = new AvatarGeneration(width, height);
-        const finalGeneration: Cell[][] = avatarGeneration.start(4);
+        const finalGeneration: Cell[][] = avatarGeneration.start(10);
         draw(ctx, finalGeneration);
     });
 
@@ -61,7 +71,6 @@
 </script>
 
 <div class="avatar">
-    <div>Random Boy {index}</div>
     <div>
         <canvas bind:this={canvasElement} height="{canvasHeight}px" width="{canvasWidth}px"></canvas>
     </div>
